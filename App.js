fd12f123721d094b6fbb43141587651b55ae6a3f
Annie Rączka <x.aniutka@gmail.com>
2025-11-12 23:45:38 +0100

    fix: navigation

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MenuScreen from './src/screens/MenuScreen';
 import GuideScreen from './src/screens/GuideScreen';
-import SongSelectScreen from './src/screens/SongSelectScreen';
+import SongSelect from './src/screens/SongSelectScreen';
+import DescSong from './src/screens/DescSong';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="Menu" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Menu" component={MenuScreen} />
         <Stack.Screen name="Guide" component={GuideScreen} />
-        <Stack.Screen name="SongSelect" component={SongSelectScreen} />
+        <Stack.Screen name="SongSelect" component={SongSelect} />
+        <Stack.Screen name="DescSong" component={DescSong} />
         </Stack.Navigator>
     </NavigationContainer>
   );
