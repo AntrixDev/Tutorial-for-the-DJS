c390839d5f42b53497a2589de08eebaa88a14f42
Annie Rączka <x.aniutka@gmail.com>
2025-11-15 21:00:27 +0100

    add: guide cards

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -4,11 +4,32 @@ import { useSharedValue } from "react-native-reanimated";
 import { Card } from "./Card";
 
 const cards = [
-  // {
-  //   source: require("../assets/guide/TUTsecond.png"),
-  // },
   {
-    source: require("../assets/guide/TUTfirst.png"),
+    source: require("../assets/guide/ninth.png"),
+  },
+  {
+    source: require("../assets/guide/eighth.png"),
+  },
+  {
+    source: require("../assets/guide/seventh.png"),
+  },
+  {
+    source: require("../assets/guide/sixth.png"),
+  },
+  {
+     source: require("../assets/guide/fifth.png"),
+   },
+  {
+     source: require("../assets/guide/fourth.png"),
+   },
+  {
+     source: require("../assets/guide/third.png"),
+   },
+  {
+     source: require("../assets/guide/second.png"),
+   },
+  {
+    source: require("../assets/guide/first.png"),
   },
 ];
 
